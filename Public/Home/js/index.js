d5f05b03dad1160e16326f1190c85f393c1185c5
win7 <896582248@qq.com>
2017-09-07 00:40:36 +0800

    index

--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -1,3 +1,34 @@
-$(function () {
-    
+$(function() {
+    $('.banner').on({
+        'swipeleft': function() {
+            if ($(this).next('.banner').length != 0) {
+                $(this).next('.banner').show();
+                $(this).next('.banner').siblings().hide();
+            }
+        },
+        'swiperight': function() {
+            if ($(this).prev('.banner').length != 0) {
+                $(this).prev('.banner').show();
+                $(this).prev('.banner').siblings().hide();
+            }
+        }
+    });
+
+
+    $('.banner1 .slide').eq(0).addClass('active').fadeIn();
+
+    $('.banner1 .left-arrow').on('tap', function() {
+        var e = $('.banner1 .slide.active');
+        if (e.prev('.slide').length) {
+            e.prev('.slide').addClass('active').fadeIn();
+            e.prev('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    })
+    $('.banner1 .right-arrow').on('tap', function() {
+        var e = $('.banner1 .slide.active');
+        if (e.next('.slide').length) {
+            e.next('.slide').addClass('active').fadeIn();
+            e.next('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    })
 });
