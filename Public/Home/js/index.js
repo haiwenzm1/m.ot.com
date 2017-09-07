ee8382f76b717db4baea4244e9a4768a26a48cba
bnqboy <Haiwen.Ji@b-and-qchina.com>
2017-09-07 20:04:19 +0800

    index

--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -1,34 +1,110 @@
 $(function() {
+	$('.banner').eq(0).addClass('active').show();
+	$('.banner1 .slide').eq(0).addClass('active').fadeIn();
+	
     $('.banner').on({
-        'swipeleft': function() {
+        'swipeleft': function(e) {
+			e.stopPropagation(); // 阻止JavaScript事件冒泡传递
             if ($(this).next('.banner').length != 0) {
-                $(this).next('.banner').show();
-                $(this).next('.banner').siblings().hide();
+				before();
+                $(this).next('.banner').addClass('active').show();
+                $(this).next('.banner').siblings().removeClass('active').hide();
+				after();
             }
         },
-        'swiperight': function() {
+        'swiperight': function(e) {
+			e.stopPropagation(); // 阻止JavaScript事件冒泡传递
             if ($(this).prev('.banner').length != 0) {
-                $(this).prev('.banner').show();
-                $(this).prev('.banner').siblings().hide();
+				before();
+                $(this).prev('.banner').addClass('active').show();
+                $(this).prev('.banner').siblings().removeClass('active').hide();
+				after();
             }
         }
     });
 
+	function after(){
+		var index = $('.banner').index($('.banner.active'));
+		if( index == 0){
+			$('.banner1 .slide').eq(0).addClass('active').show();
+		}
+		
+		if( index == 1){
+			$('.banner2 .slide').eq(0).addClass('active').show();
+		}
+		
+		if( index == 2){
+			$('.banner3 .slide').eq(0).addClass('active').show();
+		}
+	}
+	
+	function before(){
+		var index = $('.banner').index($('.banner.active'));
+		if( index == 0){
+			$('.banner1 .slide').removeClass('active').hide();
+		}
+		
+		if( index == 1){
+			$('.banner2 .slide').removeClass('active').hide();
+		}
+		
+		if( index == 2){
+			$('.banner3 .slide').removeClass('active').hide();
+		}
+	}
 
-    $('.banner1 .slide').eq(0).addClass('active').fadeIn();
-
-    $('.banner1 .left-arrow').on('tap', function() {
+    $('.banner1 .left-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
         var e = $('.banner1 .slide.active');
         if (e.prev('.slide').length) {
             e.prev('.slide').addClass('active').fadeIn();
             e.prev('.slide').siblings('.slide').removeClass('active').fadeOut();
         }
-    })
-    $('.banner1 .right-arrow').on('tap', function() {
+    });
+	
+    $('.banner1 .right-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
         var e = $('.banner1 .slide.active');
         if (e.next('.slide').length) {
             e.next('.slide').addClass('active').fadeIn();
             e.next('.slide').siblings('.slide').removeClass('active').fadeOut();
         }
-    })
+    });
+	
+	
+	$('.banner2 .left-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
+        var e = $('.banner2 .slide.active');
+        if (e.prev('.slide').length) {
+            e.prev('.slide').addClass('active').fadeIn();
+            e.prev('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    });
+	
+    $('.banner2 .right-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
+        var e = $('.banner2 .slide.active');
+        if (e.next('.slide').length) {
+            e.next('.slide').addClass('active').fadeIn();
+            e.next('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    });
+	
+	$('.banner3 .left-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
+        var e = $('.banner3 .slide.active');
+        if (e.prev('.slide').length) {
+            e.prev('.slide').addClass('active').fadeIn();
+            e.prev('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    });
+	
+    $('.banner3 .right-arrow').on('tap', function(event) {
+		event.stopPropagation(); // 阻止JavaScript事件冒泡传递
+        var e = $('.banner3 .slide.active');
+        if (e.next('.slide').length) {
+            e.next('.slide').addClass('active').fadeIn();
+            e.next('.slide').siblings('.slide').removeClass('active').fadeOut();
+        }
+    });
 });
