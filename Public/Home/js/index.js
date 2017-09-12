aa0da320c110734ebeaba78017c43c7430dc460e
bnqboy <Haiwen.Ji@b-and-qchina.com>
2017-09-12 20:06:16 +0800

    youhua

--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -1,7 +1,6 @@
 $(function () {
     $('.banner').eq(0).addClass('active').show();
-    $('.banner1 .slide').eq(0).addClass('active').fadeIn();
-
+    $('.banner1 .slide').eq(0).addClass('active');
 
     var startX;
     $('.banner').on('touchstart', function (e) {
@@ -11,14 +10,14 @@ $(function () {
 
     $('.banner').on('touchend', function (e) {
         var _touch = e.originalEvent.changedTouches[0];
-        if (_touch.pageY - startX > 30) {
+        if (_touch.pageY - startX > 10) {
             if ($(this).prev('.banner').length != 0) {
                 before();
                 $(this).prev('.banner').addClass('active').fadeIn(1000);
                 $(this).prev('.banner').siblings().removeClass('active').fadeOut(1000);
                 after();
             }
-        } else if (startX - _touch.pageY > 30) {
+        } else if (startX - _touch.pageY > 10) {
             if ($(this).next('.banner').length != 0) {
                 before();
                 $(this).next('.banner').addClass('active').fadeIn(1000);
@@ -30,9 +29,6 @@ $(function () {
 
     function after() {
         var index = $('.banner').index($('.banner.active'));
-        if (index == 0) {
-            $('.banner1 .slide').eq(0).addClass('active').show();
-        }
 
         if (index == 1) {
             $('.banner2 .slide').eq(0).addClass('active').show();
@@ -46,7 +42,7 @@ $(function () {
     function before() {
         var index = $('.banner').index($('.banner.active'));
         if (index == 0) {
-            $('.banner1 .slide').removeClass('active').hide();
+            $('.banner1-content').css({ transition: "all 700ms ease", transform: "translate3d(0px, 0px, 0px)" });
         }
 
         if (index == 1) {
@@ -61,16 +57,20 @@ $(function () {
     $('.banner1 .left-arrow').on('click', function () {
         var e = $('.banner1 .slide.active');
         if (e.prev('.slide').length) {
-            e.prev('.slide').addClass('active').fadeIn(1000);
-            e.prev('.slide').siblings('.slide').removeClass('active').fadeOut(1000);
+            e.prev('.slide').addClass('active');
+            e.prev('.slide').siblings('.slide').removeClass('active');
+            var w = 0 - $('.banner1').width() * $('.banner1-content .slide').index($('.slide.active'));
+            $('.banner1-content').css({ transition: "all 700ms ease", transform: "translate3d(" + w + "px, 0px, 0px)" });
         }
     });
 
     $('.banner1 .right-arrow').on('click', function () {
         var e = $('.banner1 .slide.active');
         if (e.next('.slide').length) {
-            e.next('.slide').addClass('active').fadeIn(1000);
-            e.next('.slide').siblings('.slide').removeClass('active').fadeOut(1000);
+            e.next('.slide').addClass('active');
+            e.next('.slide').siblings('.slide').removeClass('active');
+            var w = 0 - $('.banner1').width() * $('.banner1-content .slide').index($('.slide.active'));
+            $('.banner1-content').css({ transition: "all 700ms ease", transform: "translate3d(" + w + "px, 0px, 0px)" });
         }
     });
 
